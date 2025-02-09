f811a950820ae2d304fb281a504086c4792a649c
Lilly <Sutphinl16@gmail.com>
2025-02-08 21:58:06 -0500

    publish

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { resolve} from "path";
 
     export default defineConfig({
         build: {
+            outDir: "docs",
             rollupOptions: {
                 input: {
                     main: resolve(__dirname, "index.html"),
-                    about: resolve(__dirname, "about/index.html"),
+                    about: resolve(__dirname, "about/index.html"),  
                 },
             },
         },
